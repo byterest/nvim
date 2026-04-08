ece2d98a65f309adaf28bd0d6b825bd1fce14028
rain <luowanshun@gmail.com>
2026-04-08 04:39:14 +0000

    support status line refine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        section_separators = "",
+        component_separators = "",
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode", -- 👈 显示当前 mode
+            fmt = function(str)
+              return " " .. str -- 可选图标
+            end,
+          },
+        },
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}
