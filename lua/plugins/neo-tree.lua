c8715b65a8d9e923573586a96869365da9d97c7b
rain <luowanshun@gmail.com>
2026-06-25 14:48:55 +0800

    support leader key

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,29 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = vim.loop.cwd(), -- 可选，打开当前工作目录
+        })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    -- 可选：打开当前文件所在目录
+    {
+      "<leader>E",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          reveal = true,
+        })
+      end,
+      desc = "Explorer NeoTree (reveal file)",
+    },
+  },
   config = function()
     require("neo-tree").setup({
       filesystem = {
@@ -24,4 +47,3 @@ return {
     })
   end,
 }
-
